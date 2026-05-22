a7211ecfb9e81ed919e080c6fef0b6bd488c2fbf
Jesus Parra <jparra@autofletes.com>
2026-05-22 13:12:13 -0600

    Fix webhook validation

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,15 +1,47 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const router = express.Router();
 
+
+// VALIDACIÓN META WEBHOOK
 router.get("/webhook", (req, res) => {
-    res.send("Webhook AFC activo");
+
+    const verify_token = process.env.VERIFY_TOKEN;
+
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode && token) {
+
+        if (mode === "subscribe" && token === verify_token) {
+
+            console.log("WEBHOOK VERIFICADO");
+
+            return res.status(200).send(challenge);
+
+        } else {
+
+            return res.sendStatus(403);
+
+        }
+
+    }
+
 });
 
-router.post("/webhook", (req, res) => {
-    console.log("POST recibido desde Meta");
+
+// RECIBIR MENSAJES
+router.post("/webhook", async (req, res) => {
+
+    console.log("Mensaje recibido:");
+
     console.log(JSON.stringify(req.body, null, 2));
+
     res.sendStatus(200);
+
 });
 
 module.exports = router;
